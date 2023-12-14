dee06ab3512b7ced8ce693b66a632da7ab0abaaa
songpink <jejeje04@naver.com>
2023-12-14 20:09:27 +0900

    feat : 안내 메시지 문자열 상수로 정의

--- src/main/kotlin/menu/Model/Messages.kt
+++ src/main/kotlin/menu/Model/Messages.kt
@@ -3,7 +3,7 @@ package menu.Model
 object Messages {
     const val START_SUGGESTION = "점심 메뉴 추천을 시작합니다."
     const val INPUT_NAMES = "코치의 이름을 입력해 주세요. (, 로 구분)"
-    const val INPUT_CANNOT_EAT_MENU = "%s가 못 먹는 메뉴를 입력해 주세요."
+    const val INPUT_CANNOT_EAT_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요."
     const val THIS_IS_SUGGESTION_MENU = "메뉴 추천 결과입니다."
     const val TOP_OF_CHART = "[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"
     const val CATEGORY_OF_CHART = "[ 카테고리 | %s | %s | %s | %s | %s ]"
