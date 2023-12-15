14db221a85ca7783da45b57a03679a5785a788fb
songpink <jejeje04@naver.com>
2023-12-15 11:47:50 +0900

    feat : 예외처리를 위해 못먹는 메뉴 리스트에 넣는 방식을 변경

--- src/main/kotlin/menu/View/InputView.kt
+++ src/main/kotlin/menu/View/InputView.kt
@@ -4,21 +4,15 @@ import camp.nextstep.edu.missionutils.Console
 import menu.Model.Messages
 
 class InputView {
-    fun inputCoachName(): List<String> {
+    fun printInputCoachName() {
         println(Messages.START_SUGGESTION)
-        println()
-        println(Messages.INPUT_NAMES)
-
+        println("\n" + Messages.INPUT_NAMES)
+    }
+    fun inputCoachName(): List<String> {
         return Console.readLine().split(",").map { it.trim() }
     }
-
-    fun inputCannotEatMenus(coachNames: List<String>): MutableList<List<String>> {
-        val cannotEatMenuList: MutableList<List<String>> = mutableListOf()
-        for (i in coachNames) {
-            println(Messages.INPUT_CANNOT_EAT_MENU.format(i))
-            val temp = Console.readLine().split(",").map { it.trim() }
-            cannotEatMenuList.add(temp)
-        }
-        return cannotEatMenuList
+    fun inputCannotEatMenusByCoach(coachName: String): List<String> {
+        println("\n" + Messages.INPUT_CANNOT_EAT_MENU.format(coachName))
+        return Console.readLine().split(",").map { it.trim() }
     }
 }
