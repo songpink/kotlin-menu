85f9e3e4f7304900d46860ea41265df5ef113817
songpink <jejeje04@naver.com>
2023-12-15 20:36:41 +0900

    fix : 음식 카테고리에 매핑되는 번호 변경

--- src/main/kotlin/menu/Model/Messages.kt
+++ src/main/kotlin/menu/Model/Messages.kt
@@ -9,4 +9,9 @@ object Messages {
     const val CATEGORY_OF_CHART = "[ 카테고리 | %s | %s | %s | %s | %s ]"
     const val NAME_OF_CHART = "[ %s | %s | %s | %s | %s | %s ]"
     const val SUGGESTION_COMPLETE = "추천을 완료했습니다."
+
+    const val NUM_OF_COACHES_ERROR = "[ERROR] 코치는 2~5명이어야 합니다. 다시 입력해주세요."
+    const val THERE_IS_NO_MENU = "[ERROR] 입력한 메뉴는 존재하지 않는 메뉴입니다. 다시 입력해주세요."
+    const val NAME_LENGTH_ERROR = "[ERROR] 코치의 이름이 2~4자가 아닙니다. 다시 입력해 주세요."
+    const val CANNOT_EAT_LENGTH_ERROR = "[ERROR] 못 먹는 메뉴는 0~2개 이어야 합니다. 다시 입력해 주세요."
 }
