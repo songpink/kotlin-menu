11b9c187e0115f8035a605c894cc5159af7e9672
songpink <jejeje04@naver.com>
2023-12-15 11:48:39 +0900

    refactor : 함수가 길어져 분리

--- src/main/kotlin/menu/Controller/Run.kt
+++ src/main/kotlin/menu/Controller/Run.kt
@@ -1,18 +1,42 @@
 package menu.Controller
 
 import menu.View.InputView
+import menu.View.OutputView
 
 class Run {
-    val inputView = InputView()
-    val suggestion = Suggestion()
-    fun run(){
-        val coachNames = inputView.inputCoachName()
-        val cannotEatMenus = inputView.inputCannotEatMenus(coachNames)
-        val categoriesByDay = suggestion.decideCategory()
-        suggestion.setSuggestionMenuByDay(coachNames)
-        for (i in 0..4){
-            suggestion.decideMenu(coachNames, categoriesByDay[i])
+    private val inputView = InputView()
+    private val suggestion = Suggestion()
+    private val outputView = OutputView()
+    private val exceptionHandling = ExceptionHandling()
+    fun run() {
+        inputView.printInputCoachName()
+        var coachNames: List<String> = inputView.inputCoachName()
+        while (exceptionHandling.numOfCoachesUnderTwoErr(coachNames)) {
+            coachNames = inputView.inputCoachName()
         }
-        print(suggestion.suggestionMenuByDay)
+        val cannotEatMenus: MutableList<List<String>> = mutableListOf()
+        for (c in coachNames) {
+            var cannotEatMenusByCoach = inputView.inputCannotEatMenusByCoach(c)
+            while (exceptionHandling.cannotEatMenuErr(cannotEatMenusByCoach)) {
+                cannotEatMenusByCoach = inputView.inputCannotEatMenusByCoach(c)
+            }
+            cannotEatMenus.add(cannotEatMenusByCoach)
+        }
+        suggestionAndprintChart(coachNames, cannotEatMenus)
+    }
+
+    private fun suggestionAndprintChart(
+        coachNames: List<String>,
+        cannotEatMenus: MutableList<List<String>>
+    ) {
+        suggestion.decideCategory()
+        suggestion.setSuggestionMenuByDay(coachNames)
+        for (i in 0..4) {
+            suggestion.decideMenu(coachNames, suggestion.categoriesByDay[i], cannotEatMenus)
+        }
+        outputView.printChart(
+            suggestion.categoriesNameByDay,
+            coachNames, suggestion.suggestionMenuByDay
+        )
     }
 }
